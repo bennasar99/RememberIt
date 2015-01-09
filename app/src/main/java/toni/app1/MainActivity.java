03a80d7c89acee042b4e3f7b763c124524cb1ada
tonibm19 <tonibm19@gmail.com>
2015-01-09 21:25:23 +0100

    First commit
    Working with Android 4.4 and 5.0

---
+++ app/src/main/java/toni/app1/MainActivity.java
@@ -0,0 +1,70 @@
+package toni.app1;
+
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+import toni.RememberIt.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "toni.app1.MESSAGE";
+    public int id = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void sendMessage(View view){
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        Intent Intent = getIntent();
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle(getResources().getString(R.string.edit_message))
+                        .setContentText(message);
+        // Sets an ID for the notification
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Builds the notification and issues it.
+        mNotifyMgr.notify(id, mBuilder.build());
+        id ++;
+    }
+}
